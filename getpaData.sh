dc68b3364a6de9a971b714f4f4a2fe2ad5fec4b8
jianlianggao <j.gao@imperial.ac.uk>
2016-11-14 22:30:40 +0000

    added bash file for testing papy

---
+++ getpaData.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#for downloading data
+wget https://raw.githubusercontent.com/jianlianggao/papy/master/papy/TutorialData.csv -O /usr/local/bin/TutorialData.csv
+cd /usr/local/bin
+./pa.py
