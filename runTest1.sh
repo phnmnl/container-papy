98e1b4e126040d974b4e5006f18b6ebb95e9c2de
j <j.gao@aaa.net>
2016-11-15 11:17:39 +0000

    added runTest bash to run testing and comparison

---
+++ runTest1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#download test data
+#for downloading data
+wget https://raw.githubusercontent.com/jianlianggao/papy/master/papy/TutorialData.csv -O /usr/local/bin/TutorialData.csv
+
+#run pa.py
+cd /usr/local/bin
+
+./pa.py TutorialData.csv 8
+
+
+#download results for comparison
+wget https://raw.githubusercontent.com/jianlianggao/papy/develop/results/diffgroups_0.csv -O /usr/local/bin/diffgroups_0.csv
+wget https://raw.githubusercontent.com/jianlianggao/papy/develop/results/linearregression_0.csv -O /usr/local/bin/linearregression_0.csv
+
+
+#run comparison
+# compare output
+if ! cmp -s "diffgroups_0.csv" "diffgroups.csv"; then
+	echo "diffgroups files don't match!"
+	exit 1
+fi
+
+if ! cmp -s "linearregression_0.csv" "linearregression.csv"; then
+	echo "linearregression files don't match!"
+	exit 1
+fi
+
+echo "All files created successfully"
