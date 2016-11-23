dc2dee03194524243385b373fa9076af455d79fd
j <j.gao@aaa.net>
2016-11-23 13:13:10 +0000

    updated test script to check if a plot is generated

--- runTest1.sh
+++ runTest1.sh
@@ -11,23 +11,23 @@ cd /usr/local/bin
 
 
 #download results for comparison
-wget https://raw.githubusercontent.com/jianlianggao/papy/develop/results/diffgroups_0.csv -O /usr/local/bin/diffgroups_0.csv
-wget https://raw.githubusercontent.com/jianlianggao/papy/develop/results/linearregression_0.csv -O /usr/local/bin/linearregression_0.csv
+#wget https://raw.githubusercontent.com/jianlianggao/papy/develop/results/diffgroups_0.csv -O /usr/local/bin/diffgroups_0.csv
+#wget https://raw.githubusercontent.com/jianlianggao/papy/develop/results/linearregression_0.csv -O /usr/local/bin/linearregression_0.csv
 
 
 #run comparison
 # compare output
 
-diff "diffgroups.csv" "diffgroups_0.csv"
+#diff "diffgroups.csv" "diffgroups_0.csv"
 
-temp="$(diff "diffgroups.csv" "diffgroups_0.csv")"
+#temp="$(diff "diffgroups.csv" "diffgroups_0.csv")"
 
-if [ ! -z "$temp" ]; then 
-   echo "diffgroups are not equal"
-   exit 1
-else
-   echo "diffgroups are equal"
-fi
+#if [ ! -z "$temp" ]; then 
+#   echo "diffgroups are not equal"
+#   exit 1
+#else
+#   echo "diffgroups are equal"
+#fi
 
 #temp="$(diff "linearregression.csv" "linearregression_0.csv")"
 
@@ -37,5 +37,13 @@ fi
 #else
 #   echo "linearregression are equal"
 #fi
+## check if the file was generated
+file_name = "papy_result_surface.html"
+if [ -f "$file_name" ]; then 
+  echo "$file found."
+else
+  echo "$file not found!"
+  exit 1
+fi
 
 echo "All files created successfully"
