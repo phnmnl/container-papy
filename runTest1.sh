751b959578b39741d5dfee18d24df08053421c32
j <j.gao@aaa.net>
2017-03-01 11:57:34 +0000

    updated test file for fitting new input paramters of PAPY

--- runTest1.sh
+++ runTest1.sh
@@ -7,7 +7,7 @@ wget https://raw.githubusercontent.com/jianlianggao/papy/master/papy/TutorialDat
 #run pa.py
 cd /usr/local/bin
 
-./pa.py TutorialData.csv 8
+./pa.py TutorialData.csv 0-8 0:100:501 0.05:0.05:0.7 20
 
 
 #download results for comparison
@@ -38,7 +38,7 @@ cd /usr/local/bin
 #   echo "linearregression are equal"
 #fi
 ## check if the file was generated
-file_name="papy_result_surface.html"
+file_name="papy_output_zip.zip"
 if [ -f "$file_name" ]; then 
   echo "$file_name found."
 else
